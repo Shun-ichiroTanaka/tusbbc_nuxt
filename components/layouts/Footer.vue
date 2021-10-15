d37aa9809ff7836c25ef0abb16e95233f1320fb8
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-10-15 14:58:38 +0900

    修正

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,18 +1,33 @@
 <template>
   <!-- component -->
-  <footer class="footer relative pt-1">
-    <div class="footer-bottom lg:mt-16 container">
-      <div
-        class="footer-bottom-content flex flex-col justify-center items-center"
-      >
-        <p class="text-sm mb-2 cursor-default">
-          ©
-          <span ref="year"></span>
-          - 東京理科大学硬式野球部
-        </p>
+  <div class="">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1950.1139095626734!2d139.91201505401003!3d35.92186342847694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1634277173791!5m2!1sja!2sjp"
+      width="100%"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+    ></iframe>
+    <footer class="footer relative">
+      <div class="footer-bottom container">
+        <div
+          class="
+            footer-bottom-content
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <p class="text-sm mb-2 cursor-default">
+            ©
+            <span ref="year"></span>
+            - 東京理科大学硬式野球部
+          </p>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 <script>
 export default {
